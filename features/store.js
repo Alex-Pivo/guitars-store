7553d0a8d3c65dc409e84f84d9a573cf08d1f1a0
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-05-28 17:16:56 +0300

    Delete features directory

--- features/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import guitarsSlice from "./guitars/guitarsSlice";
-
-export const store = configureStore({
-    reducer: {
-        guitars: guitarsSlice,
-    },
-    devTools: true,
-})
