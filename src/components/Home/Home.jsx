539d08aae886049ff5ece3936241889e330bb350
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-05-28 16:52:45 +0300

    Delete src directory

--- src/components/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Body from "../../components/3dHomePage/Body";
-
-const Home = () => {
-  return <Body></Body>;
-};
-
-export default Home;
--- src/routes/routes,.js
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-  HOME: "/",
-  GUITARS: "/guitars",
-  ABOUT: "/about",
-  CATEGORY: "/guitars/:id"
-};
