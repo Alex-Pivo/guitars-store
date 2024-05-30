4eb00a4a720385c0c91c040ff05f328a8e37b62a
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-05-30 16:03:58 +0300

    comp

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,83 +1,117 @@
-import React from "react";
-import styles from "../../styles/sideBar.module.scss";
-import { NavLink } from "react-router-dom";
+// import React from "react";
+// import styles from "../../styles/sideBar.module.scss";
+// import { NavLink } from "react-router-dom";
+// import { useCart } from "../Cart/CartContext";
 
-// const Sidebar = () => {
+
+// const manufacturers = ["Усі", "Fender", "D'Angelico", "Les Paul"];
+// const guitarTypes = ["Усі", "electric", "acoustic", "bass"];
+// const fretCounts = ["Усі", "19", "20", "21", "22"];
+// const pickupTypes = ["Усі", "S", "S-S", "H", "H-H"];
+// const priceRanges = ["Усі", "0 - 10000", "10000 - 30000", "30000 - 50000", "50000 - 100000"];
+
+// const Sidebar = ({ filters, setFilters }) => {
+//   const handleFilterChange = (filterName, value) => {
+//     setFilters({ ...filters, [filterName]: value });
+//   };
+
 //   return (
-//     <>
-//       <div className={styles.sidebar}>
-//         <div className={styles.title}>Фільтри</div>
-//         <nav>
-//           <ul className={styles.menu}>
-//             <li className={styles.box}>
-//               <p className={styles.name}>Виробник</p>
-//               <select className={styles.select} name="firm" id="1">
-//                 <option value="value1" selected>Усі</option>
-//                 <option value="value2" >
-//                   Fender
+//     <div className={styles.sidebar}>
+//       <div className={styles.title}>Фільтри</div>
+//       <nav>
+//         <ul className={styles.menu}>
+//           <li className={styles.box}>
+//             <p className={styles.name}>Виробник</p>
+//             <select
+//               className={styles.select}
+//               name="manufacturer"
+//               onChange={(e) => handleFilterChange("manufacturer", e.target.value)}
+//               value={filters.manufacturer}
+//             >
+//               {manufacturers.map((manufacturer, index) => (
+//                 <option key={index} value={manufacturer}>
+//                   {manufacturer}
 //                 </option>
-//                 <option value="value3">D'Angelico</option>
-//                 <option value="value3">Les Paul</option>
-//               </select>
-
-//             </li>
-
-//             <li className={styles.box}>
-//               <p className={styles.name}>Тип</p>
-//               <select className={styles.select} name="type" id="1">
-//                 <option value="value1" selected>Усі</option>
-//                 <option value="value2" >
-//                   Електрогітари
+//               ))}
+//             </select>
+//           </li>
+//           <li className={styles.box}>
+//             <p className={styles.name}>Тип гітари</p>
+//             <select
+//               className={styles.select}
+//               name="type"
+//               onChange={(e) => handleFilterChange("type", e.target.value)}
+//               value={filters.type}
+//             >
+//               {guitarTypes.map((type, index) => (
+//                 <option key={index} value={type}>
+//                   {type}
 //                 </option>
-//                 <option value="value3">Акустичні гітари</option>
-//                 <option value="value3">Бас гітари</option>
-//               </select>
-//             </li>
-
-//             <li className={styles.box}>
-//               <p className={styles.name}>Кількість ладів</p>
-//               <select className={styles.select} name="lads" id="1">
-//                 <option value="value1" selected>Усі</option>
-//                 <option value="value2" >
-//                   21
+//               ))}
+//             </select>
+//           </li>
+//           <li className={styles.box}>
+//             <p className={styles.name}>Кількість ладів</p>
+//             <select
+//               className={styles.select}
+//               name="frets"
+//               onChange={(e) => handleFilterChange("frets", e.target.value)}
+//               value={filters.frets}
+//             >
+//               {fretCounts.map((fretCount, index) => (
+//                 <option key={index} value={fretCount}>
+//                   {fretCount}
 //                 </option>
-//                 <option value="value3">22</option>
-//                 <option value="value3">24</option>
-//               </select>
-//             </li>
-
-//             <li className={styles.box}>
-//               <p className={styles.name}>Матеріал деки</p>
-//               <select className={styles.select} name="deka" id="1">
-//                 <option value="value1" selected>Усі</option>
-//                 <option value="value2" >
-//                   21
+//               ))}
+//             </select>
+//           </li>
+//           <li className={styles.box}>
+//             <p className={styles.name}>Тип звукоснімачів</p>
+//             <select
+//               className={styles.select}
+//               name="pickupType"
+//               onChange={(e) => handleFilterChange("pickupType", e.target.value)}
+//               value={filters.pickupType}
+//             >
+//               {pickupTypes.map((pickupType, index) => (
+//                 <option key={index} value={pickupType}>
+//                   {pickupType}
 //                 </option>
-//                 <option value="value3">22</option>
-//                 <option value="value3">24</option>
-//               </select>
-//             </li>
-
-//             <li className={styles.box}>
-//               <p className={styles.name}>Матеріал грифа</p>
-//               <select className={styles.select} name="grif" id="1">
-//                 <option value="value1" selected>Усі</option>
-//                 <option value="value2" >
-//                   21
+//               ))}
+//             </select>
+//           </li>
+//           <li className={styles.box}>
+//             <p className={styles.name}>Ціна</p>
+//             <select
+//               className={styles.select}
+//               name="price"
+//               onChange={(e) => handleFilterChange("price", e.target.value)}
+//               value={filters.price}
+//             >
+//               {priceRanges.map((priceRange, index) => (
+//                 <option key={index} value={priceRange}>
+//                   {priceRange}
 //                 </option>
-//                 <option value="value3">22</option>
-//                 <option value="value3">24</option>
-//               </select>
-//             </li>
-//           </ul>
-//         </nav>
-//       </div>
-//     </>
+//               ))}
+//             </select>
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
 //   );
 // };
 
-import { useCart } from "../Cart/CartContext";
+// export default Sidebar;
+
+
+import React from "react";
+import styles from "../../styles/sideBar.module.scss";
+
 const manufacturers = ["Усі", "Fender", "D'Angelico", "Les Paul"];
+const guitarTypes = ["Усі", "electric", "acoustic", "bass"];
+const fretCounts = ["Усі", "19", "20", "21", "22"];
+const pickupTypes = ["Усі", "S", "S-S", "H", "H-H"];
+const priceRanges = ["Усі", "0 - 10000", "10000 - 30000", "30000 - 50000", "50000 - 100000"];
 
 const Sidebar = ({ filters, setFilters }) => {
   const handleFilterChange = (filterName, value) => {
@@ -85,81 +119,88 @@ const Sidebar = ({ filters, setFilters }) => {
   };
 
   return (
-    <>
-      <div className={styles.sidebar}>
-        <div className={styles.title}>Фільтри</div>
-        <nav>
-          <ul className={styles.menu}>
-            <li className={styles.box}>
-              <p className={styles.name}>Виробник</p>
-              <select
-                className={styles.select}
-                name="firm"
-                id="1"
-                onChange={(e) =>
-                  handleFilterChange("manufacturer", e.target.value)
-                }
-                value={filters.manufacturer}
-              >
-                {manufacturers.map((manufacturer, index) => (
-                  <option key={index} value={manufacturer}>
-                    {manufacturer}
-                  </option>
-                ))}
-              </select>
-            </li>
-            <li className={styles.box}>
-              <p className={styles.name}>Тип гітари</p>
-              <select
-                className={styles.select}
-                name="type"
-                id="2"
-                onChange={(e) => handleFilterChange("type", e.target.value)}
-                value={filters.type}
-              >
-                {/* Варианты выбора типа гитар */}
-                <option value="Усі">Усі</option>
-                <option value="electric">Electric</option>
-                <option value="acoustic">Acoustic</option>
-                <option value="bass">Bass</option>
-              </select>
-            </li>
-            <li className={styles.box}>
-              <p className={styles.name}>Кількість ладів</p>
-              <select
-                className={styles.select}
-                name="type"
-                id="2"
-                onChange={(e) => handleFilterChange("type", e.target.value)}
-                value={filters.type}
-              >
-                {/* Варианты выбора типа гитар */}
-                <option value="Усі">Усі</option>
-                <option value="electric">Electric</option>
-                <option value="acoustic">Acoustic</option>
-                <option value="bass">Bass</option>
-              </select>
-            </li>
-            <li className={styles.box}>
-              <p className={styles.name}>Ціна</p>
-              <select
-                className={styles.select}
-                name="type"
-                id="2"
-                onChange={(e) => handleFilterChange("type", e.target.value)}
-                value={filters.type}
-              >
-                {/* Варианты выбора типа гитар */}
-                <option value="Усі">Усі</option>
-                <option value="electric">Electric</option>
-                <option value="acoustic">Acoustic</option>
-                <option value="bass">Bass</option>
-              </select>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <div className={styles.sidebar}>
+      <div className={styles.title}>Фільтри</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li className={styles.box}>
+            <p className={styles.name}>Виробник</p>
+            <select
+              className={styles.select}
+              name="manufacturer"
+              onChange={(e) => handleFilterChange("manufacturer", e.target.value)}
+              value={filters.manufacturer}
+            >
+              {manufacturers.map((manufacturer, index) => (
+                <option key={index} value={manufacturer}>
+                  {manufacturer}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li className={styles.box}>
+            <p className={styles.name}>Тип гітари</p>
+            <select
+              className={styles.select}
+              name="type"
+              onChange={(e) => handleFilterChange("type", e.target.value)}
+              value={filters.type}
+            >
+              {guitarTypes.map((type, index) => (
+                <option key={index} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li className={styles.box}>
+            <p className={styles.name}>Кількість ладів</p>
+            <select
+              className={styles.select}
+              name="fret"
+              onChange={(e) => handleFilterChange("fret", e.target.value)}
+              value={filters.fret}
+            >
+              {fretCounts.map((fretCount, index) => (
+                <option key={index} value={fretCount}>
+                  {fretCount}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li className={styles.box}>
+            <p className={styles.name}>Тип звукоснімачів</p>
+            <select
+              className={styles.select}
+              name="pickupType"
+              onChange={(e) => handleFilterChange("pickupType", e.target.value)}
+              value={filters.pickupType}
+            >
+              {pickupTypes.map((pickupType, index) => (
+                <option key={index} value={pickupType}>
+                  {pickupType}
+                </option>
+              ))}
+            </select>
+          </li>
+          <li className={styles.box}>
+            <p className={styles.name}>Ціна</p>
+            <select
+              className={styles.select}
+              name="price"
+              onChange={(e) => handleFilterChange("price", e.target.value)}
+              value={filters.price}
+            >
+              {priceRanges.map((priceRange, index) => (
+                <option key={index} value={priceRange}>
+                  {priceRange}
+                </option>
+              ))}
+            </select>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
---
+++ src/components/SliderComp/SliderComp.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import 'swiper/css/effect-fade';
+import "swiper/css/thumbs";
+
+import {EffectFade, Autoplay } from "swiper/modules";
+import styles from "../../styles/slidercomp.module.scss";
+
+const SliderComp = () => {
+  return (
+    <>
+      <div className={styles.swiper__container}>
+        <Swiper
+          style={{
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+            backgroundColor: "#ffffff",
+          }}
+          effect={'fade'}
+          spaceBetween={0}
+          autoplay={{
+            delay: 6500,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectFade, Autoplay]}
+          className={styles.swiper}
+        >
+          <SwiperSlide>
+            <div
+              className={styles.img}
+              style={{
+                width: "100%",
+                height: "100%",
+                background: `url("/slider/fender.jpg")`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <div className={styles.shadow}>
+                <h2>FENDER</h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className={styles.img}
+              style={{
+                width: "100%",
+                height: "100%",
+                background: `url("/slider/dangelico.jpg")`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <div className={styles.shadow}>
+                <h2>D'ANGELICO</h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className={styles.img}
+              style={{
+                width: "100%",
+                height: "100%",
+                background: `url("/slider/les.jpg")`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <div className={styles.shadow}>
+                <h2>LES PAUL</h2>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className={styles.img}
+              style={{
+                width: "100%",
+                height: "100%",
+                background: `url("/slider/jackson.png")`,
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
+              <div className={styles.shadow}>
+                <h2>JACKSON</h2>
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default SliderComp;
