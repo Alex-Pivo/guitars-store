9d2c5515909d52936d7f2a3f1f46e1c29e25c736
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-06-04 02:09:02 +0300

    Update sendEmail.js

--- src/api/sendEmail.js
+++ src/api/sendEmail.js
@@ -1,42 +1,34 @@
-const express = require('express');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+export default async function sendEmail(req, res) {
+  if (req.method === 'POST') {
+    const { to, subject, html } = req.body;
 
-app.use(bodyParser.json());
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'pivoshenckoalexsey@gmail.com',
+        pass: '120pio603A',
+      },
+    });
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'pivoshenckoalexsey@gmail.com',
-    pass: '120pio603A',
-  },
-});
+    const mailOptions = {
+      from: 'pivoshenckoalexsey@gmail.com',
+      to,
+      subject,
+      html,
+    };
 
-app.post('/api/send-email', (req, res) => {
-  const { to, subject, html } = req.body;
-
-  const mailOptions = {
-    from: 'pivoshenckoalexsey@gmail.com',
-    to,
-    subject,
-    html,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send('Error sending email');
-    } else {
+    try {
+      const info = await transporter.sendMail(mailOptions);
       console.log('Email sent:', info.response);
       res.status(200).send('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
     }
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  } else {
+    res.status(405).send({ error: 'Method Not Allowed' });
+  }
+}
 
